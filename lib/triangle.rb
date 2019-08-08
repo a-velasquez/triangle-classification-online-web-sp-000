737ea7379f4e828779798e8d247dc9623613d3c7
Alex Velasquez <jtinnesco@gmail.com>
2019-08-08 05:35:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,11 @@
 class Triangle
-  attr_reader :side1, :side2, :side3 
-  
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3 
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
-  
+
   def kind
     validate_triangle
     if a == b && b == c
@@ -17,4 +16,16 @@ class Triangle
       :scalene
     end
   end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
